2aeba19c0eb0c119f7d397f598bd570c8fd5ff43
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2025-06-22 09:42:57 +0200

    minor

--- apps/todo-pro-trpc-server/src/entities/client.ts
+++ apps/todo-pro-trpc-server/src/entities/client.ts
@@ -5,31 +5,22 @@ const localHost = 'db'; // or 'localhost'
 const TABLE_NAME = 'yetanotherapp-xyz-data-table';
 const TEST_TABLE_NAME = 'yetanotherapp-xyz-data-test-table';
 
-const localDBClient = new DynamoDBClient({
+const localDBConfig = {
   region: localHost,
   endpoint: `http://${localHost}:8000`,
   credentials: {
     accessKeyId: 'sao9c6', // Can be anything for local
     secretAccessKey: 'ixxi48',
   },
-});
+};
+const localDBClient = new DynamoDBClient(localDBConfig);
+const localDocumentClient = new DynamoDB.DocumentClient(localDBConfig);
 
-const localDocumentClient = new DynamoDB.DocumentClient({
-  region: localHost,
-  endpoint: `http://${localHost}:8000`,
-  credentials: {
-    accessKeyId: 'sao9c6', // Can be anything for local
-    secretAccessKey: 'ixxi48',
-  },
-});
-
-const prodDBClient = new DynamoDBClient({
+const prodDBConfig = {
   region: 'eu-central-1',
-});
-
-const prodDocumentClient = new DynamoDB.DocumentClient({
-  region: 'eu-central-1',
-});
+};
+const prodDBClient = new DynamoDBClient(prodDBConfig);
+const prodDocumentClient = new DynamoDB.DocumentClient(prodDBConfig);
 
 export const getClient = () => {
   if (['development', 'test'].includes(process.env.NODE_ENV ?? '') || process.env.APP_ENVIRONMENT === 'development') {
